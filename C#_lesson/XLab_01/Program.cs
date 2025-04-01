95ff72c9e91f5fd48844a231effa8aedff7c6ce3
Daniel Sher <d.sheremetev@carx-tech.com>
2025-04-01 16:40:44 +0300

    Project Xlab_01 & Xlab_02

---
+++ C#_lesson/XLab_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_XLab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        //создаю переменные
+        short sh = 0; //-32678
+        int i = 0; //16777215
+        long lo = 0;
+        float fl = 0;
+        double d1 = 0;
+        object obj1 = d1;
+        double d2 = (double)obj1;
+            //задаю для i,obj значение
+            int i = 1;
+        Console.WriteLine(i);
+            //задаю для obj значение из i
+            object obj = i; //боксинг
+            Console.WriteLine(obj);
+            //перевожу обджект в индекс и присваиваю в i
+            int i = (int)obj; //анбоксинг
+            Console.WriteLine(i)
+    }
+}
---
+++ C#_lesson/XLab_02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XLab_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создаю простые переменные
+            short s = 0;
+            int i = 0;
+            long lo = 0;
+            float fl = 0;
+            double d = 0;
+
+            //создаю объект
+            object obj1 = d; //боксинг
+            double d2 = (double)d; //анбоксинг
+
+
+        }
+    }
+}
